a419cb75580fb488596dba71d3ee335635105efb
onpine <718215627@qq.com>
2021-08-20 11:33:30 +0800

    feat:有计划的状态管理器

---
+++ demo-2/index.js
@@ -0,0 +1,25 @@
+import createStore from "./createStore.js";
+import reducer from "./reducer.js";
+
+let initState = {
+  count: 0,
+};
+/*把plan函数*/
+let store = createStore(reducer, initState);
+
+store.subscribe(() => {
+  let state = store.getState();
+  console.log(state.count);
+});
+/*自增*/
+store.dispatch({
+  type: "INCREMENT",
+});
+/*自减*/
+store.dispatch({
+  type: "DECREMENT",
+});
+/*我想随便改 计划外的修改是无效的！*/
+store.dispatch({
+  count: "abc",
+});
---
+++ demo-2/reducer.js
@@ -0,0 +1,19 @@
+/*注意：action = {type:'',other:''}, action 必须有一个 type 属性*/
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "DECREMENT":
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    default:
+      return state;
+  }
+}
+
+export default reducer;
